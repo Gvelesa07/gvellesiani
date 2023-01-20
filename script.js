0c9b279d976b809e489a1f24367c9dc3e5d019de
Gvelesa07 <116925513+Gvelesa07@users.noreply.github.com>
2023-01-20 16:44:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const toDoList = [
+    {
+        id: 1,
+        title: "my todo item"
+    },
+    {
+        id: 2,
+        title: "second todo item"
+    },
+    {
+        id: 3,
+        title: "third todo item"
+    },
+]
+const ul = document.getElementById('todolist'); 
+const btn = document.getElementById('btn')
+const inpt = document.getElementById('inpput')
+
+ function addItem()
+{
+    countries.push(input.value)
+    input.value = null
+    renderList()
+}
+toDoList.forEach(item => console.log(item.id))
+
